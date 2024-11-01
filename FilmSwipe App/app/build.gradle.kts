086570854d69afe1ac1096aaf3923ebbf0cb9489
a3-milner <arthur2.milner@uwe.ac.uk>
2024-11-01 14:19:48 +0000

    Added retrofit dependencies

--- FilmSwipe App/app/build.gradle.kts
+++ FilmSwipe App/app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation(libs.retrofit)
     implementation (libs.converter.gson)
     implementation (libs.logging.interceptor)
+    implementation (libs.androidx.lifecycle.livedata)
 }
